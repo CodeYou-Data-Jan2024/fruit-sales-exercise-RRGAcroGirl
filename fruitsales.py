625539a993fd6c7c94c46a253c1f0297deac8107
Nicole Meyer <meyer.k.nicole@gmail.com>
2024-04-28 16:02:19 -0400

    Fixed transposed values

--- fruitsales.py
+++ fruitsales.py
@@ -1,7 +1,7 @@
 # import pandas
 import pandas as pd
 #create a data frame with the excerise's specified requirements
-fruit_sales = pd.DataFrame({'Apples': [35, 21], 'Bananas': [41, 34]}, index=['2017 Sales', '2018 Sales'])
+fruit_sales = pd.DataFrame({'Apples': [35, 41], 'Bananas': [21, 34]}, index=['2017 Sales', '2018 Sales'])
 
 #save data frame to a csv
 fruit_sales.to_csv('fruit.csv')
